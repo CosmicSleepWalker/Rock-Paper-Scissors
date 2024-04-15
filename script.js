9293e404cec8d51d4aa287178f04b92b5fc862ae
CosmicSleepWalker <dev.Amadeush@gmail.com>
2024-04-15 00:51:01 -0500

    Created playRound function that is used to decide who wins the game

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+
 let getComputerChoice = () => {
     let randomNum = Math.floor(Math.random() * 3);
-    console.log(randomNum);
     switch(randomNum){
         case 0:
-            return "Rock";
+            return ROCK;
         case 1:
-            return "Paper";
+            return PAPER;
         case 2:
-            return "Scissors";
+            return SCISSORS;
     }
 };
 
+let playRound = (playerSelection, computerSelection) => {
+    let losePlayer = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    let winPlayer = `You Win! ${playerSelection} beats ${computerSelection}`;
+    let tie = "You Tied!";
+    if(playerSelection === SCISSORS && computerSelection === ROCK || playerSelection === PAPER && computerSelection === SCISSORS || playerSelection === ROCK && computerSelection === PAPER){
+        return losePlayer;
+    }else if(computerSelection === playerSelection){
+        return tie;
+    }else{
+        return winPlayer;
+    }
+};
+
+let playerSelection = "Paper".toUpperCase();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
